88e35d55eb6cdefb9037c7c1f9b58df2c4183693
Lavell Wesley <lavellthedev+github@gmail.com>
2020-03-29 02:29:24 -0400

    algorithim practice

--- Beginner/vowelsCounter/index-START.js
+++ Beginner/vowelsCounter/index-START.js
@@ -4,8 +4,20 @@ e.g vowelsCounter('anehizxcv') // will return 3
 */
 
 
+//An Iterative Approach//
+
+const vowels = ["a", "e", "i", "o", "u"]
 function vowelsCounter(text) {
     // Code goes here
+    let count = 0 ;
+
+    for (let letter of text.toLowerCase()){
+      if(vowels.includes(letter)){
+        counter ++
+      }
+    }
+
+    return counter
 }
 
 
