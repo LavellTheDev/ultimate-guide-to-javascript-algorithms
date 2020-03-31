80cdf86faee5759a61250bcae6de2aa114d15ce5
Lavell Wesley <lavellthedev+github@gmail.com>
2020-03-30 20:10:38 -0400

    changes

--- Beginner/maxRecurringChar/index-START.js
+++ Beginner/maxRecurringChar/index-START.js
@@ -5,46 +5,43 @@ e.g maxRecurringChar('aabacada') // will return 'a'
 
 
 
-// function maxRecurringChar(text) {
-//     let charMap = {}
-//     let maxCharValue = 0
-//     let maxChar = ''
-//
-//     for(let char of text){
-//       if (charMap.hasOwnProperty(char)){
-//         charMap[char]++
-//       }else {
-//         charMap[char] = 1;
-//       }
-//     }
-//     for(let char in charMap){
-//       if(charMap[char] > maxCharValue){
-//         maxCharValue = charMap[char]
-//         maxChar = char
-//       }
-//     }
-//     return maxChar
-// }
-
-//Forming Arrays from th Character Map
 function maxRecurringChar(text) {
     let charMap = {}
-    let charArray = []
-    let vaulesArray = []
     let maxCharValue = 0
-
-    for(let char of text){
-      if(charMap.hasOwnProperty(char)){
-        charMap[char++]
-      }else{
-        charMap[char]=1
-      }
+    let maxChar = ''
+    for (let char of text) {
+        if (charMap.hasOwnProperty(char)) {
+            charMap[char]++
+        } else {
+            charMap[char] = 1
+        }
     }
-    charArray = object.keys(charMap)
-    valuesArray = object.values(charMap)
-     maxCharValue = Math.max(...vaulesArray)
-
-     return charArray[vaulesArray.indexOf(maxCharValue)]
+    for (let char in charMap) {
+        if (charMap[char] > maxCharValue) {
+            maxCharValue = charMap[char]
+            maxChar = char
+        }
+    }
+    return maxChar
 }
 
-module.exports = maxRecurringChar;
+// FORMING ARRAYS FROM THE CHARACTER MAP METHOD
+function maxRecurringChar(text) {
+    let charMap = {}
+    let charArray =[]
+    let valuesArray = []
+    let maxCharValue = 0
+
+    for (let char of text) {
+        if (charMap.hasOwnProperty(char)) {
+            charMap[char]++
+        } else {
+            charMap[char] = 1
+        }
+    }
+    charArray = Object.keys(charMap)
+    vaulesArray = Object.values(charMap)
+    maxCharValue = Math.max(...vaulesArray)
+
+    return charArray[vaulesArray.indexOf(maxCharValue)]
+}
